b6cc8f5aadbd9a62d796ae2a67328278b826c1f1
benat merino <ikctr@plaiaundi.net>
2024-05-21 08:48:18 +0200

    mejorado

--- src/main/java/Servlet/CaballeroServlet.java
+++ src/main/java/Servlet/CaballeroServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import modelo.bean.Caballero;
+import modelo.dao.CaballeroDAO;
 
 /**
  * Servlet implementation class CaballeroServlet
@@ -31,8 +32,8 @@ public class CaballeroServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		 ArrayList<Caballero> caballeros = caballeroDAO.getAllCaballeros();
+		CaballeroDAO caballeroDAO = new CaballeroDAO();
+		 ArrayList<Caballero> caballeros = (ArrayList<Caballero>) caballeroDAO.getAllCaballeros();
 	        request.setAttribute("caballeros", caballeros);
 	        request.getRequestDispatcher("/verCaballeros.jsp").forward(request, response);
 	}
